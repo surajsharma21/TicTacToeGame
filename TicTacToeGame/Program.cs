1fd65ed9ad7aa0fa32a28df426e9eb319dc3a160
Suraj <surajshrm000@gmail.com>
2020-10-13 09:50:20 +0530

    [Suraj] Add: Create a new game board

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe!");
+            TicTacToeGame game = new TicTacToeGame();
+            game.CreateBoard();
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToeGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+     class TicTacToeGame
+    {
+        public char[] board;
+        public TicTacToeGame()
+        {
+            board = new char[10];
+        }
+        public void CreateBoard()
+        {
+            TicTacToeGame game = new TicTacToeGame();
+            for (int position = 1; position < 10; position++)
+            {
+                game.board[position] = ' ';
+            }
+        }
+    }
+}
